9834b2e528a01a77df71cf295f1e6f26fa82ef83
Guilherme Torres <guilhermetorres97@gmail.com>
2017-10-24 10:56:48 -0200

    bugs in random generation and wall/door placement

--- Slime Wars/levels/level-manager.gd
+++ Slime Wars/levels/level-manager.gd
@@ -5,6 +5,8 @@ var number_of_rooms = 10
 var level_matrix = []
 
 var rooms = []
+var door = preload("res://nodes/scenario/open-door.tscn")
+var not_door = preload("res://nodes/scenario/wall2.tscn")
 
 func _ready():
 	randomize()
@@ -12,6 +14,7 @@ func _ready():
 	generate_rooms()
 
 func generate_rooms():
+	var room_list = []
 	for i in range(level_dimension):
 		var new = []
 		for j in range(level_dimension):
@@ -27,5 +30,48 @@ func generate_rooms():
 			var new_room = rooms[randi()% rooms.size()].instance()
 			new_room.set_global_pos(Vector2(pos[0] * global.ROOM_WIDTH, pos[1] * global.ROOM_HEIGHT))
 			add_child(new_room)
+			room_list.append(new_room)
 		pos[randi()%2] += modifiers[randi() % modifiers.size()]
-	
+	for it in level_matrix:
+		print(it)
+	# adiciona portas
+	for it in room_list:
+		var it_positions = it.get_node("positions")
+		var it_doors = it.get_node("doors")
+		var it_walls = it.get_node("walls")
+		if (it.get_global_pos().y != 0) && (level_matrix[it.get_global_pos().x/global.ROOM_WIDTH][it.get_global_pos().y/global.ROOM_HEIGHT - 1] == 1):
+			var new_door = door.instance()
+			new_door.set_global_pos(it_positions.get_children()[0].get_pos())
+			it_doors.add_child(new_door)
+			print(new_door.get_global_pos())
+#		else:
+#			var new = not_door.instance()
+#			new.set_global_pos(it_positions.get_children()[0].get_pos())
+#			it_walls.add_child(new)
+		if (it.get_global_pos().x != (level_dimension-1) * global.ROOM_WIDTH) && (level_matrix[it.get_global_pos().x/global.ROOM_WIDTH + 1][it.get_global_pos().y/global.ROOM_HEIGHT] == 1):
+			var new_door = door.instance()
+			new_door.set_global_pos(it_positions.get_children()[1].get_pos())
+			new_door.set_rot(deg2rad(90))
+			it_doors.add_child(new_door)
+#		else:
+#			var new = not_door.instance()
+#			new.set_global_pos(it_positions.get_children()[1].get_pos())
+#			it_walls.add_child(new)
+		if (it.get_global_pos().y != (level_dimension-1) * global.ROOM_HEIGHT) && (level_matrix[it.get_global_pos().x/global.ROOM_WIDTH][it.get_global_pos().y/global.ROOM_HEIGHT + 1] == 1):
+			var new_door = door.instance()
+			new_door.set_global_pos(it_positions.get_children()[2].get_pos())
+			new_door.set_rot(deg2rad(180))
+			it_doors.add_child(new_door)
+#		else:
+#			var new = not_door.instance()
+#			new.set_global_pos(it_positions.get_children()[2].get_pos())
+#			it_walls.add_child(new)
+		if (it.get_global_pos().x != 0) && (level_matrix[it.get_global_pos().x/global.ROOM_WIDTH - 1][it.get_global_pos().y/global.ROOM_HEIGHT] == 1):
+			var new_door = door.instance()
+			new_door.set_global_pos(it_positions.get_children()[3].get_pos())
+			new_door.set_rot(deg2rad(270))
+			it_doors.add_child(new_door)
+#		else:
+#			var new = not_door.instance()
+#			new.set_global_pos(it_positions.get_children()[3].get_pos())
+#			it_walls.add_child(new)
--- Slime Wars/nodes/rooms/room-manager.gd
+++ Slime Wars/nodes/rooms/room-manager.gd
@@ -4,12 +4,10 @@ onready var camera = get_node("Camera2D")
 onready var player = get_node("../").get_node("player")
 onready var doors = get_node("doors")
 onready var enemies = get_node("enemies")
-var ROOM_WIDTH = 1408
-var ROOM_HEIGHT = 896
 
 func _ready():
 	set_process(true)
 
 func _process(delta):
-	if abs((get_global_pos() - player.get_global_pos()).x) < ROOM_WIDTH/2 - 50 && abs((get_global_pos() - player.get_global_pos()).y) < ROOM_HEIGHT/2 - 50:
+	if abs((get_global_pos() - player.get_global_pos()).x) < global.ROOM_WIDTH/2 - 50 && abs((get_global_pos() - player.get_global_pos()).y) < global.ROOM_HEIGHT/2 - 50:
 		camera.make_current()
